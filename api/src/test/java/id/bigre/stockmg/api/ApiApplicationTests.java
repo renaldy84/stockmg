d9689fdf57ba266df30910b0a3435ca0439afcf6
Renaldy <dufronte@gmail.com>
2024-07-20 21:40:04 +0700

    initial commit

---
+++ api/src/test/java/id/bigre/stockmg/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.bigre.stockmg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
